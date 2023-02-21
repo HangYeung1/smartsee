da9ce57e635f3b52df3b3636ebf0e1f8427b1170
Hang Yeung <yeung.hang@gmail.com>
2023-02-21 00:28:58 -0500

    Update navigation bar

--- app/navigation/MainContainer.js
+++ app/navigation/MainContainer.js
@@ -1,50 +1,63 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 // Screens
-import CameraScreen from './screens/CameraScreen';
-import SearchScreen from './screens/SearchScreen';
-import RecentsScreen from './screens/RecentsScreen';
+import CameraScreen from "./screens/CameraScreen";
+import SearchScreen from "./screens/SearchScreen";
+import RecentsScreen from "./screens/RecentsScreen";
 
 // Screen names
-const cameraName = 'Camera';
-const searchName = 'Search';
-const recentsName = 'Recents';
+const cameraName = "Camera";
+const searchName = "Search";
+const recentsName = "Recents";
 
 const Tab = createBottomTabNavigator();
 
+export default function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={cameraName}
+        screenOptions={
+          // Tab bar styling
+          ({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let routeName = route.name;
 
-export default function MainContainer(){
-    return(
-        <NavigationContainer>
-            <Tab.Navigator
-            initialRouteName={cameraName}
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
-                    let routeName = route.name;
+              if (routeName === cameraName) {
+                iconName = focused ? "camera" : "camera-outline";
+              } else if (routeName === searchName) {
+                iconName = focused ? "search" : "search-outline";
+              } else if (routeName === recentsName) {
+                iconName = focused ? "time" : "time-outline";
+              }
+              return <Ionicons name={iconName} size={size + 5} color={color} />;
+            },
+            tabBarActiveTintColor: "mediumseagreen",
+            tabBarInactiveTintColor: "darkgrey",
+            tabBarStyle: {
+              height: 90,
+              backgroundColor: "black",
+              borderTopWidth: 0,
+            },
+            tabBarLabelStyle: {
+              fontSize: 10,
+              marginTop: -10,
+            },
 
-                    if(routeName === cameraName) {
-                        iconName = focused ? 'camera' : 'camera-outline'
-                    }
-                    else if(routeName === searchName) {
-                        iconName = focused ? 'search' : 'search-outline'
-                    }
-                    else if(routeName === recentsName) {
-                        iconName = focused ? 'time' : 'time-outline'
-                    }
-                    return <Ionicons name={iconName} size={size} color={color} />
-                },
-                tabBarActiveTintColor: 'seagreen',
-                tabBarInactiveTintColor: 'grey',
-            })}>
-            
-            <Tab.Screen name={searchName} component={SearchScreen} />
-            <Tab.Screen name={cameraName} component={CameraScreen} />
-            <Tab.Screen name={recentsName} component={RecentsScreen} />
-
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+            // Hide the header
+            headerShown: false,
+          })
+        }
+      >
+        <Tab.Screen name={searchName} component={SearchScreen} />
+        <Tab.Screen name={cameraName} component={CameraScreen} />
+        <Tab.Screen name={recentsName} component={RecentsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
